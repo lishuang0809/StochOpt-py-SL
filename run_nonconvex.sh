c4c2d5ab86ce8b3605fd2e928e3429a9c63faf4e
lishuang0809 <lishuang0809@gmail.com>
2022-05-12 14:50:28 -0600

    Added Rosenbrock loss

--- run_nonconvex.sh
+++ run_nonconvex.sh
@@ -28,40 +28,45 @@
 
 # To be updated!
 
-regs=(0.0)
-#regs=(0.000 0.001 0.008)
-#betas=(0.0 0.3 0.5 0.7)
-#regs=(0.000 0.001 0.002 0.003 0.004 0.005 0.006 0.007 0.008 0.009)
-betas=(0.0 0.3) # 0.5 0.7
-DATASET=("colon-cancer") #mushrooms duke colon-cancer
-NUM_regs=${#regs[@]} 
-NUM_betas=${#betas[@]}
-NUM_DATASETS=${#DATASET[@]}
 
 
-for (( j=0; j<$NUM_DATASETS; j++ ))
-do
-    for (( r=0; r<$NUM_betas; r++ ))
-    do
-        for (( s=0; s<$NUM_regs; s++ ))
-        do
-            NAME="${DATASET[j]}-${betas[r]}"
-            python3 main.py --type 1 --dataset ${DATASET[j]} --data_path "./datasets/${DATASET[j]}" \
-                        --name $NAME --result_folder 'sps2_exact_max' --log_file "log-${NAME}.txt" \
-                        --epochs 60 --n_repetition 1 --reg ${regs[s]} --tol 1e-2 --lamb 0.1  \
-                        --loss "Logistic" --regularizer 'L2' --beta ${betas[r]} --run_SP2max True --run_SP2maxp True
-                        #              --run_sps True --run_sgd True --run_SP2 True  \
-  # --run_sps True --run_sgd True --run_adam True --run_sps2 True --run_sps2slack True --run_sps2 True --run_SP2L2p True --run_SP2L1p True --run_sps True --run_sgd True --run_adam True --run_SP2maxp True
-   #    echo "Finished regularization ${regs[s]}"
-        done
-        
-#        utils.plot_grad_time(result_dict=dict_time_iter, problem=data_set, title = opt.name + "-time-iter-reg" + str(reg), save_path=folder_path)
-        
 
-    done
 
-    echo "Finished ${DATASET[j]}"
-done
+
+#regs=(0.0)
+##regs=(0.000 0.001 0.008)
+##betas=(0.0 0.3 0.5 0.7)
+##regs=(0.000 0.001 0.002 0.003 0.004 0.005 0.006 0.007 0.008 0.009)
+#betas=(0.0 0.3) # 0.5 0.7
+#DATASET=("colon-cancer") #mushrooms duke colon-cancer
+#NUM_regs=${#regs[@]}
+#NUM_betas=${#betas[@]}
+#NUM_DATASETS=${#DATASET[@]}
+#
+#
+#for (( j=0; j<$NUM_DATASETS; j++ ))
+#do
+#    for (( r=0; r<$NUM_betas; r++ ))
+#    do
+#        for (( s=0; s<$NUM_regs; s++ ))
+#        do
+#            NAME="${DATASET[j]}-${betas[r]}"
+#            python3 main.py --type 1 --dataset ${DATASET[j]} --data_path "./datasets/${DATASET[j]}" \
+#                        --name $NAME --result_folder 'sps2_exact_max' --log_file "log-${NAME}.txt" \
+#                        --epochs 60 --n_repetition 1 --reg ${regs[s]} --tol 1e-2 --lamb 0.1  \
+#                        --loss "Logistic" --regularizer 'L2' --beta ${betas[r]} --run_SP2max True --run_SP2maxp True
+#                        #              --run_sps True --run_sgd True --run_SP2 True  \
+#  # --run_sps True --run_sgd True --run_adam True --run_sps2 True --run_sps2slack True --run_sps2 True --run_SP2L2p True --run_SP2L1p True --run_sps True --run_sgd True --run_adam True --run_SP2maxp True
+#   #    echo "Finished regularization ${regs[s]}"
+#        done
+#
+##        utils.plot_grad_time(result_dict=dict_time_iter, problem=data_set, title = opt.name + "-time-iter-reg" + str(reg), save_path=folder_path)
+#
+#
+#    done
+#
+#    echo "Finished ${DATASET[j]}"
+#done
 
 #python plottime.py --regs ${regs} --betas ${betas}     
 #echo "Finished plotting time"
